6c6a9c2d084968119c635e42fcf2d4d8e631e9fc
Rahul K P <rahulkp01aug@gmail.com>
2023-06-09 11:01:48 +0530

    Removed default newline character at the end of echo by adding -n flag

--- frontend.sh
+++ frontend.sh
@@ -7,22 +7,22 @@ LOGFILE=/tmp/$COMPONENT.log
 
 echo -e "\e[92m############ $COMPONENT Installation started ############\e[0m"
 
-echo -e "\e[36m Installing Nginx\e[0m"
+echo -ne "\e[36m Installing Nginx\e[0m"
 yum install nginx -y	&>>LOGFILE
 systemctl enable nginx	&>>LOGFILE
 systemctl start nginx	&>>LOGFILE
 status $?
 
-echo -e "\e[36m Downloading the $COMPONENT component\e[0m"
+echo -ne "\e[36m Downloading the $COMPONENT component\e[0m"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 status $?
 
-echo -e "\e[36m Performing cleanup\e[0m"
+echo -ne "\e[36m Performing cleanup\e[0m"
 cd /usr/share/nginx/html
 rm -rf *
 status $?
 
-echo -e "\e[36m Extracting \e[0m"
+echo -ne "\e[36m Extracting \e[0m"
 unzip /tmp/frontend.zip		&>>LOGFILE
 mv frontend-main/* .
 mv static/* .
@@ -30,7 +30,7 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 status $?
 
-echo -e "\e[36m Starting Nginx server \e[0m"
+echo -ne "\e[36m Starting Nginx server \e[0m"
 systemctl daemon-reload
 systemctl enable nginx	
 systemctl restart nginx	
