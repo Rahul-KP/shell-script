61527087293e6bad0ccdfa20e94f0d6b2e63f5cd
Rahul K P <rahulkp01aug@gmail.com>
2023-06-09 10:40:51 +0530

    Added code to push stdout to logfile, yet to ckeck if its working

--- frontend.sh
+++ frontend.sh
@@ -3,32 +3,37 @@
 source ./common.sh
 
 COMPONENT=frontend
+LOGFILE=/tmp/$COMPONENT.log
 
 echo -e "\e[92m############ $COMPONENT Installation started ############\e[0m"
 
 echo -e "\e[36m Installing Nginx\e[0m"
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
+yum install nginx -y	&>>LOGFILE
+systemctl enable nginx	&>>LOGFILE
+systemctl start nginx	&>>LOGFILE
 status $?
 
 echo -e "\e[36m Downloading the $COMPONENT component\e[0m"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+status $?
 
 echo -e "\e[36m Performing cleanup\e[0m"
 cd /usr/share/nginx/html
 rm -rf *
+status $?
 
 echo -e "\e[36m Extracting \e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip		&>>LOGFILE
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+status $?
 
 echo -e "\e[36m Starting Nginx server \e[0m"
 systemctl daemon-reload
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx	
+systemctl restart nginx	
+status $?
 
 echo -e "\e[92m############ $COMPONENT Installation Complete ############\e[0m"
