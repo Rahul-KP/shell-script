c7a2ecba7fe3d04a008917d2667aac098639871b
Rahul K P <rahulkp01aug@gmail.com>
2023-06-09 10:44:27 +0530

    Fixed priniting issue for status(), -e flag for escape characters was not specified

--- common.sh
+++ common.sh
@@ -2,8 +2,8 @@
 
 status() {
 	if [ $1 -eq 0 ] ; then
-		echo -n "\e[32msuccess\e[0m"
+		echo -ne "\e[32msuccess\e[0m"
 	else
-		echo -n "\e[31mfailure\e[0m"
+		echo -ne "\e[31mfailure\e[0m"
 	fi
 }
