ef645d3dc735ec529ec52cbdc2a16da4b7ea5e84
Rahul K P <rahulkp01aug@gmail.com>
2023-06-14 23:38:09 +0530

    auto push

--- common.sh
+++ common.sh
@@ -105,7 +105,7 @@ PYTHON() {
     DOWNLOAD_AND_EXTRACT         # calling DOWNLOAD_AND_EXTRACT  function download the content
 
     echo -n "Installing $COMPONENT"
-    cd /home/${USER}/${COMPONENT}/
+    cd /home/${USER}/${COMPONENT}-main/
     pip3 install -r requirements.txt    &>> $LOGFILE 
     status $?
 
@@ -113,7 +113,7 @@ PYTHON() {
     GROUPID=$(id -g roboshop) 
 
     echo -n "Updating the uid and gid in the $COMPONENT.ini file"
-    sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  /home/${USER}/${COMPONENT}/${COMPONENT}.ini
+    sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  /home/${USER}/${COMPONENT}-main/${COMPONENT}.ini
     
     CONFIGURE_SVC
 
