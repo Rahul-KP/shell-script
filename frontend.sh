ba31d7dd3e8710f11d2c03ca0da99c789518db4b
Rahul K P <rahulkp01aug@gmail.com>
2023-06-14 13:59:35 +0530

    Added cart to reverse proxy in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -30,7 +30,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 status $?
 
 echo -ne "\e[36m Configuring reverse proxy settings \e[0m"
-for component in catalogue user; do
+for component in catalogue user cart; do
     sed -i -e "/$component/s/localhost/$component.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
 done
 status $?
