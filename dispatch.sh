c1c769943b1fef126c6f41dac2c892e2c070a76c
Rahul K P <rahulkp01aug@gmail.com>
2023-06-15 00:05:13 +0530

    auto push

--- dispatch.sh
+++ dispatch.sh
@@ -7,7 +7,7 @@ source ./common.sh
 echo -e "\e[92m############ $COMPONENT Installation started ############\e[0m"
 
 echo -ne "\e[36m Installing Goloang \e[0m"
-yum install golang -y
+yum install golang -y &>>$LOGFILE
 status $?
 
 CREATE_USER
@@ -19,4 +19,3 @@ go get
 go build
 
 CONFIGURE_SERVICE
-echo -e "\e[92m############ $COMPONENT Installation complete ############\e[0m"
