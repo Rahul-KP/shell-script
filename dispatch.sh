ab1ff5d2746d048c4304390e295147ea3e7ac6ef
Rahul K P <rahulkp01aug@gmail.com>
2023-06-14 23:59:22 +0530

    Added dispatch.sh

---
+++ dispatch.sh
@@ -0,0 +1,22 @@
+#! bin/bash
+
+COMPONENT=dispatch
+
+source ./common.sh
+
+echo -e "\e[92m############ $COMPONENT Installation started ############\e[0m"
+
+echo -ne "\e[36m Installing Goloang \e[0m"
+yum install golang -y
+status $?
+
+CREATE_USER
+
+DOWNLOAD_AND_EXTRACT
+mv dispatch-main dispatch 
+go mod init dispatch
+go get 
+go build
+
+CONFIGURE_SERVICE
+echo -e "\e[92m############ $COMPONENT Installation complete ############\e[0m"
