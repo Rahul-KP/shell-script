6d43ce18f7a79cd71527890e37ea7ee4434bc78c
Rahul K P <rahulkp01aug@gmail.com>
2023-06-14 09:49:39 +0530

    Auto push

--- redis.sh
+++ redis.sh
@@ -7,7 +7,7 @@ source ./common.sh
 echo -e "\e[92m############ $COMPONENT Installation started ############\e[0m"
 
 echo -ne "\e[36m Adding redis repo to yum package manager \e[0m"
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -s -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 status $?
 
 echo -ne "\e[36m Installing $COMPONENT \e[0m"
@@ -19,7 +19,7 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/$COMPONENT.conf /etc/$COMPONENT/$COMPONENT
 status $?
 
 echo -ne "\e[36m Starting $COMPONENT \e[0m"
-systemctl enable redis
+systemctl enable redis &>>$LOGFILE
 systemctl restart redis
 status $?
 
